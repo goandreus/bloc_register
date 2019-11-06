04069e7e49003b127e76cf91274dc82bb923956d
goandreus <andreshtml5@gmail.com>
2019-11-05 22:50:28 -0500

    providers

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -10,7 +10,7 @@ class Provider extends InheritedWidget {
   factory Provider({ Key key, Widget child }) {
 
     if ( _instancia == null ) {
-      _instancia = new Provider._internal(key: key, child: child );
+      _instancia =  Provider._internal(key: key, child: child );
     }
 
     return _instancia;
